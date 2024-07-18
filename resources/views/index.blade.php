6f54557cc9a6c5355c0157ddd0ad711c9449cf9d
Karen Ballesteros <ballesteroskp.179.stud@cdd.edu.ph>
2024-07-18 16:39:17 +0800

    trial2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Karen Ballesteros</title>
+</head>
+<body>
+    <h1> ITP17 ADVANCED PROGRAMMING</h1>
+
+    <a href="{{ url('/posts') }}">POST</a>
+    
+</body>
+</html>
